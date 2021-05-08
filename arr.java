31d9b6ebadd3ba8ee698616293e2c4363ef3eb2a
Riteshborole <83824820+Riteshborole@users.noreply.github.com>
2021-05-08 14:56:47 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ public class arr
+ 
+ {
+   public static void main(String args[])
+    
+	{
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("enter value;");
+	   int a1[]=new int[5];
+	   for(int i=0;i<=4;i++)
+	   {
+	      a1[i]=sc.nextInt();
+	   }
+     for(int i=0;i<=4;i++)
+	 {
+	    System.out.println(a1[i]+" ");
+	 }
+	 }
+}	 
